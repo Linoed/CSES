2c3ed11ad31371ef23d48604e5b65b43f20bf337
Linoed <gary.mac@iinet.com.au>
2023-05-12 18:07:24 +1000

    Add Subarray Sums I

---
+++ subarray_sums1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main(void) {
+
+    int n, x;
+    cin >> n >> x;
+
+    unordered_map<long long, int> mp;
+    mp.reserve(1 << 19);
+    mp[0] = 1;
+
+    long long sum = 0;
+    long long res = 0;
+    int a;
+    for (int i = 0; i < n; ++i) {
+        cin >> a;
+        sum += a;
+        res += mp[sum - x];
+        mp[sum]++;
+    }
+    cout << res << '\n';
+
+}
